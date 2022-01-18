92949cdc695ce511d585d8392277a0acc78f8f25
tyomhk <cheertrain15@naver.com>
2022-01-18 23:12:35 +0900

    Changed the look of the page and visualize the result of gulp tasks.

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,5 @@ import { randomNum } from "./util";
 const randomNumOne = randomNum(11);
 const randomNumTwo = randomNum(22);
 const randomNumThree = randomNum(33);
+
+console.log(randomNumOne, randomNumTwo, randomNumThree);
